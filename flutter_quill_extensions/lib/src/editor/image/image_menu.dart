bf0d6e1ac730a5810354232e59d85599d70409e3
David Wagner <david@dwagner.one>
2025-02-19 14:37:39 +0100

    fix: removed unused import

--- flutter_quill_extensions/lib/src/editor/image/image_menu.dart
+++ flutter_quill_extensions/lib/src/editor/image/image_menu.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart' show showCupertinoModalPopup;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart'
-    show ImageUrl, QuillController, StyleAttribute, getEmbedNode;
+    show QuillController, StyleAttribute, getEmbedNode;
 import 'package:flutter_quill/internal.dart';
 import 'package:path/path.dart' as p;
 import 'package:url_launcher/url_launcher.dart';
